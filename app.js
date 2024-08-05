b5686c00961330497517b3863fd29c85ae64db66
Pablo de Andrade Silva <pablodeas@gmail.com>
2024-08-04 22:35:16 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+// TODO: Refatorar todo o código, ta muito embolado
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirTextonaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2})
+}
+
+function exibirMensagemInicial() {
+    exibirTextonaTela('h1', 'Jogo do Número Secreto');
+    exibirTextonaTela('p', 'Escolha um número entre 1 a 10');
+}
+
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+    // O valor 10 se remete a quantidade de valores sorteados possíveis
+    if (quantidadeDeElementosNaLista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    }
+    
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio;
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        return numeroEscolhido;
+    }
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio;
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto) {
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativas'
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}.`
+        exibirTextonaTela('h1', 'Acertou!');
+        exibirTextonaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextonaTela('p', 'O número secreto é Menor.');
+        } else {
+            exibirTextonaTela('p', 'O número secreto é Maior.')
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+exibirMensagemInicial();
